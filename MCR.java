fa83e1bc4bef43e389da41877de009b154ace417
Jordan Pryor <xxxfearxxxfatal@gmail.com>
2013-01-08 11:57:57 -0600

    Base Mod file done added Copper ore registers

--- MCR.java
+++ MCR.java
@@ -4,10 +4,34 @@ pacage JordanP.MinecraftRevamped.com;
 
 
 //Mod info
-@
-
+@Mod(modid = "JordanP_MCR", name = "Minecraft Revamped", version = "In-Dev 0.0.1")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
 
 public class MCR
 {
-  @Instance(")
-  
+
+@Instance("JordanP_MCR", "Minecraft Revamped")
+public static MCR instance;
+
+//Block Static(S)
+public static Block oreCopper;
+
+@PreInit
+public void load(FMLPreInitializationEvent e)
+{
+
+//block inputs
+oreCopper = new BlockCopperOre(230).setStepSound(Block.soundStoneFootstep).setHardness(3F).setResistance(1.0F).setBlockName("oreCopper").setCreativeTab(CreativeTabs.tabBlock);
+
+//game registers (blocks only)
+GameRegistry.registerBlock(oreCopper);
+
+//language registry (blocks and items)
+LanguageRegistry.addName(oreCopper, "Copper Ore");
+
+ }
+ @cpw.mods.fml.common.Mod.PreInit
+ public void PreInit(FMLPreInitializationEvent event){
+ System.out.println("Minecraft Revamped Loaded Successfully!");
+  }
+}
